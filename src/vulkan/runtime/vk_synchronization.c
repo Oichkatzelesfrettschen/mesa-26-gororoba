joshuaswarren/mesa-1

vulkan: add missing VkMemoryRangeBarriersInfoKHR support
This has been introduced by VK_KHR_device_address_commands and we
missed it completely.

Backport-to: 26.1
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41198>

src/vulkan/runtime/vk_synchronization.c
@@ -99,6 +99,14 @@ vk_collect_dependency_info_src_stages(const VkDependencyInfo* pDependencyInfo)
       stages |= pDependencyInfo->pBufferMemoryBarriers[i].srcStageMask;
    for (uint32_t i = 0; i < pDependencyInfo->imageMemoryBarrierCount; i++)
       stages |= pDependencyInfo->pImageMemoryBarriers[i].srcStageMask;
+
+   const VkMemoryRangeBarriersInfoKHR *mem_barriers_info =
+      vk_find_struct_const(pDependencyInfo->pNext, MEMORY_RANGE_BARRIERS_INFO_KHR);
+   if (mem_barriers_info) {
+      for (uint32_t i = 0; i < mem_barriers_info->memoryRangeBarrierCount; i++)
+         stages |= mem_barriers_info->pMemoryRangeBarriers[i].srcStageMask;
+   }
+
    return stages;
 }
 

Oichkatzelesfrettschen/mesa-26-gororoba

(cherry picked from commit ce60e18bce1a7564a8c8052d6a336c743f594270)
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41268>

src/vulkan/runtime/vk_synchronization.c
@@ -120,6 +120,14 @@ vk_collect_dependency_info_dst_stages(const VkDependencyInfo* pDependencyInfo)
       stages |= pDependencyInfo->pBufferMemoryBarriers[i].dstStageMask;
    for (uint32_t i = 0; i < pDependencyInfo->imageMemoryBarrierCount; i++)
       stages |= pDependencyInfo->pImageMemoryBarriers[i].dstStageMask;
+
+   const VkMemoryRangeBarriersInfoKHR *mem_barriers_info =
+      vk_find_struct_const(pDependencyInfo->pNext, MEMORY_RANGE_BARRIERS_INFO_KHR);
+   if (mem_barriers_info) {
+      for (uint32_t i = 0; i < mem_barriers_info->memoryRangeBarrierCount; i++)
+         stages |= mem_barriers_info->pMemoryRangeBarriers[i].dstStageMask;
+   }
+
    return stages;
 }
 